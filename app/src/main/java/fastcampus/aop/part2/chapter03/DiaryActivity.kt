a187a9b60ee35492dc2156ac8588fcaae26ccc77
kimminkyu <kmk50009@naver.com>
2021-04-29 14:20:32 +0900

    Secret Diary 기능 구현함

---
+++ app/src/main/java/fastcampus/aop/part2/chapter03/DiaryActivity.kt
@@ -0,0 +1,42 @@
+package fastcampus.aop.part2.chapter03
+
+import android.content.Context
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import androidx.core.widget.addTextChangedListener
+
+class DiaryActivity: AppCompatActivity(){
+
+    private val handler = Handler(Looper.getMainLooper())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary)
+
+        val diaryEditText = findViewById<EditText>(R.id.diaryEditText)
+
+        val detailPreferences = getSharedPreferences("diary", Context.MODE_PRIVATE)
+
+        diaryEditText.setText(detailPreferences.getString("detail",""))
+
+        val runnable = Runnable {
+            getSharedPreferences("diary", Context.MODE_PRIVATE).edit{
+                putString("detail", diaryEditText.text.toString())
+            }
+
+            Log.d("DiaryActivity", "SAVE!! ${diaryEditText.text.toString()}")
+        }
+
+        diaryEditText.addTextChangedListener {
+
+            Log.d("DiaryActivity", "TextChanged : $it")
+            handler.removeCallbacks(runnable)
+            handler.postDelayed(runnable, 500)
+        }
+    }
+}
--- app/src/main/java/fastcampus/aop/part2/chapter03/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part2/chapter03/MainActivity.kt
@@ -1,11 +1,114 @@
 package fastcampus.aop.part2.chapter03
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.NumberPicker
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.edit
 
 class MainActivity : AppCompatActivity() {
+
+    private val numberPicker1: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker1)
+            .apply{
+                minValue = 0
+                maxValue = 9
+            }
+    }
+
+    private val numberPicker2: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker2)
+            .apply{
+                minValue = 0
+                maxValue = 9
+            }
+    }
+
+    private val numberPicker3: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker3)
+            .apply{
+                minValue = 0
+                maxValue = 9
+            }
+    }
+
+    private val openButton: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.openButton)
+    }
+
+    private val changePasswordButton: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.changePasswordButton)
+    }
+
+    private var changePasswordMode = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numberPicker1
+        numberPicker2
+        numberPicker3
+
+        openButton.setOnClickListener{
+            if(changePasswordMode){
+                Toast.makeText(this, "비밀번호 변경 중입니다.",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
+
+            val passwordFromUser = "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+
+            if(passwordPreferences.getString("password", "000").equals(passwordFromUser)){
+                startActivity(Intent(this, DiaryActivity::class.java))
+            }else{
+                showErrorAlertDialog()
+            }
+
+            changePasswordButton.setOnClickListener{
+                val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
+                val passwordFromUser = "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+
+                if(changePasswordMode){
+                    //번호를 저장하는 기능
+
+                    passwordPreferences.edit(true) {
+                        putString("password", passwordFromUser)
+                    }
+                    changePasswordMode = false
+                    changePasswordButton.setBackgroundColor(Color.BLACK)
+                }else{
+                    //changePasswordMode 활성화 : 비밀번호가 맞는지 체크
+
+                    if(passwordPreferences.getString("password", "000").equals(passwordFromUser)){
+
+                        changePasswordMode = true
+                        Toast.makeText(this, "변경할 패스워드를 입력해주세요", Toast.LENGTH_SHORT).show()
+
+                        changePasswordButton.setBackgroundColor(Color.RED)
+
+                    }else{
+                        showErrorAlertDialog()
+                    }
+                }
+            }
+
+        }
+    }
+
+    private fun showErrorAlertDialog(){
+        AlertDialog.Builder(this)
+            .setTitle("실패")
+            .setMessage("비밀번호가 잘못되었습니다.")
+            .setPositiveButton("확인"){ _,_ -> }
+            .create()
+            .show()
+
     }
 }
